69854d55f677a6f45e8bd6dc8205e549fda2bfbf
Bordunos <Mr.bordunos@mail.ru>
2022-04-05 11:50:45 +0300

    222

--- GeekBrainsOOPHomework_3.cpp
+++ GeekBrainsOOPHomework_3.cpp
@@ -4,13 +4,13 @@
 class IFigure 
 {
 public:
-
+    virtual void FigureArea() = 0;
 };
 //abstract clas is a class that has no implementation
 class Figure
 {
 public:
-    int Square;
+    int Area;
 };
 
 
